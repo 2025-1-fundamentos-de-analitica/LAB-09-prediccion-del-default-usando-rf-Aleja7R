f4fa04fec99cf7e0494eb4b76d5d37e2d626f5b7
Aleja Rojas <arojasq@unal.edu.co>
2025-06-30 21:34:12 -0500

    Sol1 Lab9

--- homework/homework.py
+++ homework/homework.py
@@ -92,3 +92,215 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+
+# Carga de librerias
+import pandas as pd 
+from sklearn.model_selection import train_test_split, GridSearchCV 
+from sklearn.compose import ColumnTransformer 
+from sklearn.pipeline import Pipeline 
+from sklearn.preprocessing import OneHotEncoder, StandardScaler 
+from sklearn.ensemble import RandomForestClassifier 
+import pickle
+import numpy as np
+
+# Carga de datos
+train_data_zip = 'files/input/train_data.csv.zip'
+test_data_zip = 'files/input/test_data.csv.zip'
+
+# Extraccion de los datos de los archivos zip
+train_data=pd.read_csv(
+    train_data_zip,
+    index_col=False,
+    compression='zip')
+
+test_data=pd.read_csv(
+    test_data_zip,
+    index_col=False,
+    compression='zip')
+
+# Renombrar la columna "default payment next month" a "default"
+train_data.rename(columns={'default payment next month': 'default'}, inplace=True)
+test_data.rename(columns={'default payment next month': 'default'}, inplace=True)
+
+# Remover la columna "ID"
+train_data.drop(columns='ID', inplace=True)
+test_data.drop(columns='ID', inplace=True)
+
+# Recodificar la variable EDUCATION: 0 es "NaN"
+train_data['EDUCATION'] = train_data['EDUCATION'].replace(0, np.nan)
+test_data['EDUCATION'] = test_data['EDUCATION'].replace(0, np.nan)
+
+# Recodificar la variable MARRIAGE: 0 es "NaN"
+
+train_data['MARRIAGE'] = train_data['MARRIAGE'].replace(0, np.nan)
+test_data['MARRIAGE'] = test_data['MARRIAGE'].replace(0, np.nan)
+
+# Eliminar los registros con informacion no disponible (es decir, con al menos una columna con valor nulo)
+train_data.dropna(inplace=True)
+test_data.dropna(inplace=True)
+
+# Agrupar los valores de EDUCATION > 4 en la categoria "others"
+train_data.loc[train_data['EDUCATION'] > 4, 'EDUCATION'] = 4
+test_data.loc[test_data['EDUCATION'] > 4, 'EDUCATION'] = 4
+
+# Reclasificar las variables PAY_i, EDUCATION, SEX y MARRIAGE a categoricas
+# cat_columns = ['PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6', 'EDUCATION', 'SEX', 'MARRIAGE']
+cat_columns = ['EDUCATION', 'SEX', 'MARRIAGE']
+for col in cat_columns:
+    train_data[col] = train_data[col].astype('category')
+    test_data[col] = test_data[col].astype('category')
+
+# # Balancear las muestras de entrenamiento con respecto a la variable objetivo
+
+# from imblearn.under_sampling import RandomUnderSampler
+
+# # Balancear las muestras de entrenamiento con respecto a la variable objetivo
+# rus = RandomUnderSampler(random_state=42)
+
+# x_train, y_train = rus.fit_resample(train_data.drop(columns='default'), train_data['default'])
+
+# Paso 2.
+# Divida los datasets en x_train, y_train, x_test, y_test.
+#
+x_train = train_data.drop(columns='default')
+y_train = train_data['default']
+# Separar las variables categoricas
+x_train_cat = x_train.select_dtypes(include='category')
+x_train_num = x_train.select_dtypes(exclude='category')
+
+x_test = test_data.drop(columns='default')
+y_test = test_data['default']
+#
+# Paso 3.
+# Cree un pipeline para el modelo de clasificación. Este pipeline debe
+# contener las siguientes capas:
+# - Transforma las variables categoricas usando el método
+#   one-hot-encoding. (SEX, EDUCATION, MARRIAGE, PAY_0, PAY_2, PAY_3, PAY_4, PAY_5, PAY_6)
+# - Ajusta un modelo de bosques aleatorios (rando forest).
+#
+
+cat_transformer = Pipeline(steps=[
+    ('encoder', OneHotEncoder(drop='first', handle_unknown='ignore'))
+])
+
+num_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', num_transformer, x_train_num.columns),
+        ('cat', cat_transformer, x_train_cat.columns)
+    ]
+)
+
+pipeline = Pipeline([
+    ('preprocessor', preprocessor),
+    ('model', RandomForestClassifier())
+])
+
+# # Crear el pipeline
+# pipeline = Pipeline([
+#     ('encoder', OneHotEncoder(drop='first')),
+#     ('model', RandomForestClassifier())
+# ])
+
+#
+# Paso 4.
+# Optimice los hiperparametros del pipeline usando validación cruzada.
+# Use 10 splits para la validación cruzada. Use la función de precision
+# balanceada para medir la precisión del modelo.
+#
+
+param_grid = {
+    'model__n_estimators': [50, 100, 200],
+    'model__max_depth': [5, 10, 20]
+}
+
+model= GridSearchCV(pipeline, param_grid, cv=10, scoring='balanced_accuracy')
+
+model.fit(x_train, y_train)
+
+#
+# Paso 5.
+# Guarde el modelo (comprimido con gzip) como "files/models/model.pkl.gz".
+# Recuerde que es posible guardar el modelo comprimido usanzo la libreria gzip.
+#
+
+with open('files/models/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+#
+# Paso 6.
+# Calcule las metricas de precision, precision balanceada, recall,
+# y f1-score para los conjuntos de entrenamiento y prueba.
+# Guardelas en el archivo files/output/metrics.json. Cada fila
+# del archivo es un diccionario con las metricas de un modelo.
+# Este diccionario tiene un campo para indicar si es el conjunto
+# de entrenamiento o prueba. Por ejemplo:
+#
+
+# Cargo el modelo
+
+with open('files/models/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Calculo las metricas
+from sklearn.metrics import precision_score, balanced_accuracy_score, recall_score, f1_score, confusion_matrix
+
+y_train_pred = model.predict(x_train)
+y_test_pred = model.predict(x_test)
+
+metrics_train = {
+    'type': 'metrics',
+    'dataset': 'train',
+    'precision': precision_score(y_train, y_train_pred),
+    'balanced_accuracy': balanced_accuracy_score(y_train, y_train_pred),
+    'recall': recall_score(y_train, y_train_pred),
+    'f1_score': f1_score(y_train, y_train_pred)
+}
+
+metrics_test = {
+    'type': 'metrics',
+    'dataset': 'test',
+    'precision': precision_score(y_test, y_test_pred),
+    'balanced_accuracy': balanced_accuracy_score(y_test, y_test_pred),
+    'recall': recall_score(y_test, y_test_pred),
+    'f1_score': f1_score(y_test, y_test_pred)
+}
+
+
+# {'dataset': 'train', 'precision': 0.8, 'balanced_accuracy': 0.7, 'recall': 0.9, 'f1_score': 0.85}
+# {'dataset': 'test', 'precision': 0.7, 'balanced_accuracy': 0.6, 'recall': 0.8, 'f1_score': 0.75}
+#
+#
+# Paso 7.
+# Calcule las matrices de confusion para los conjuntos de entrenamiento y
+# prueba. Guardelas en el archivo files/output/metrics.json. Cada fila
+# del archivo es un diccionario con las metricas de un modelo.
+# de entrenamiento o prueba. Por ejemplo:
+#
+# {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
+# {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
+#
+
+cm_train = confusion_matrix(y_train, y_train_pred)
+cm_test = confusion_matrix(y_test, y_test_pred)
+
+cm_matrix_train = {
+    'type': 'cm_matrix',
+    'dataset': 'train',
+    'true_0': {"predicted_0": cm_train[0, 0], "predicted_1": cm_train[0, 1]},
+    'true_1': {"predicted_0": cm_train[1, 0], "predicted_1": cm_train[1, 1]}
+}
+
+cm_matrix_test = {
+    'type': 'cm_matrix',
+    'dataset': 'test',
+    'true_0': {"predicted_0": cm_test[0, 0], "predicted_1": cm_test[0, 1]},
+    'true_1': {"predicted_0": cm_test[1, 0], "predicted_1": cm_test[1, 1]}
+}
+
+# Guardar las metricas
+metrics = [metrics_train, metrics_test, cm_matrix_train, cm_matrix_test]
+pd.DataFrame(metrics).to_json('files/output/metrics.json', orient='records', lines=True)
